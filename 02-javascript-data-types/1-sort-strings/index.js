019d667884716e0429ba06219afa7cd3c0408761
laleniko <laleniko@gmail.com>
2022-11-21 00:28:11 +0300

    HW2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,13 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  const array = [...arr];
+  const compareString = (str1, str2) => {
+    return str1.localeCompare(str2, ["ru", "en"], { caseFirst: "upper" });
+  };
 
+  return array.sort((str1, str2) =>
+    param === "asc" ? compareString(str1, str2) : compareString(str2, str1)
+  );
 }
